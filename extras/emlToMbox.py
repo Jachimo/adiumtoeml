98fdb2a775108d17cc2f169666efd5432dd83bb4
Jachimo <Jachimo@users.noreply.github.com>
2021-12-20 01:45:39 -0500

    Updated based on Github user dkav's revisions to original Gist
    Updated version:
    https://gist.github.com/dkav/e0c2e0498d557425fe36f8cb832b2a60

--- extras/emlToMbox.py
+++ extras/emlToMbox.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-""" Converts a directory full of .eml files to a single Unix "mbox" file.
+"""Converts a directory full of .eml files to a single Unix "mbox" file.
 This is similar to http://www.cosmicsoft.net/emlxconvert.html
 
 Accepts as input either an individual .eml file or a directory containing one
@@ -17,63 +17,66 @@ import os
 import sys
 import mailbox
 
-global debug
-debug = True
+DEBUG = True
 
-def main( arguments ):
-    infile_name = arguments[1]
-    dest_name = arguments[2]
+
+def main(args):
+    infile_name = args[1]
+    dest_name = args[2]
     
-    if debug:
+    if DEBUG:
         print("Input is:  " + infile_name)
         print("Output is: " + dest_name)
     
-    dest_mbox = mailbox.mbox(dest_name, create=True) # if dest doesn't exist create it
-    dest_mbox.lock() # lock the mbox file
+    dest_mbox = mailbox.mbox(dest_name, create=True)  # if dest doesn't exist create it
+    dest_mbox.lock()  # lock the mbox file
     
     if os.path.isdir(infile_name):
-        if debug:
+        if DEBUG:
             print("Detected directory as input, using directory mode")
         count = 0
         for filename in os.listdir(infile_name):
-            if filename.split('.')[-1] == "eml":
+            if os.path.splitext(filename)[-1] == ".eml":
                 try:
                     fi = open(os.path.join(infile_name, filename), 'r')
-                except:
+                except OSError:
                     sys.stderr.write("Error while opening " + filename + "\n")
                     dest_mbox.close()
                     raise
-                addFileToMbox( fi, dest_mbox )
+                add_msg_to_mbox(fi, dest_mbox)
                 count += 1
                 fi.close()
-        if debug:
-            print("Processed " + str(count) + " total files.")
-    
-    if infile_name.split('.')[-1] == "eml":
-        if debug:
+        if DEBUG:
+            print("Processed " + str(count) + " total messages.")
+
+    elif os.path.splitext(infile_name)[-1] == ".eml":
+        if DEBUG:
             print("Detected .eml file as input, using single file mode")
         try:
             fi = open(infile_name, 'r')
-        except:
+        except OSError:
             sys.stderr.write("Error while opening " + infile_name + "\n")
             dest_mbox.close()
             raise
-        addFileToMbox( fi, dest_mbox )
+        add_msg_to_mbox(fi, dest_mbox)
         fi.close()
-    
-    dest_mbox.close() # close/unlock the mbox file
+
+    dest_mbox.flush()
+    dest_mbox.close()  # close/unlock the mbox file
     return 0
 
-def addFileToMbox( fi, dest_mbox ):
-    # Any additional preprocessing logic goes here...
+
+def add_msg_to_mbox(fi, dest_mbox):
+    """Add a message file to a destination mailbox"""
     try:
-        dest_mbox.add( fi )
-    except:
+        dest_mbox.add(fi)
+    except mailbox.Error:
         dest_mbox.close()
         raise
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.stderr.write("Usage: ./emlToMbox.py input outbox.mbox\n")
         sys.exit(1)
-    sys.exit( main( sys.argv ) )
+    sys.exit(main(sys.argv))
